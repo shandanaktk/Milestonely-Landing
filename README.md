6f0e7af2abd70a75be1a3021dfd81f4b39a92f53
shandanaktk <143800788+shandanaktk@users.noreply.github.com>
2025-12-30 15:31:19 +0500

    basic

--- README.md
+++
@@ -1,2 +0,0 @@
-# Milestonely-Landing
-Landing Page
---
+++ landing/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="landing-container">
+      <h1>Welcome to Milestonely</h1>
+      <input type="email" placeholder="Enter your email" />
+    </div>
+  )
+}
+
+export default App
